722374a9a5d824edd94daf27a558f4c5e79bb3d7
antoni saputra <antonisaputra049@gmail.com>
2023-06-25 23:03:39 +0700

    add middleware

--- src/lib/create_server.ts
+++ src/lib/create_server.ts
@@ -3,9 +3,11 @@ import { default as authRouter } from '@router/auth_router';
 import bodyParser from 'body-parser';
 import express from 'express';
 import http from 'http';
+import cors from 'cors';
 
 const createServer = () => {
   const app = express();
+  app.use(cors());
   app.use(bodyParser.json());
   app.use(authRouter);
   return new http.Server(app);
--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -1,15 +1,31 @@
 import { RegisterBodyRequest } from '@interface/auth.interface';
 import createResponse from '@lib/auth_response';
+import chalk from 'chalk';
 import { NextFunction, Request, Response } from 'express';
 import validator from 'validator';
 
 export default class AuthMiddleware {
+  // eslint-disable-next-line consistent-return
   static async userRegisterFormat(
     req: Request,
     res: Response,
     next: NextFunction
-  ) {
+  ): Promise<void> {
     const data: RegisterBodyRequest = await req.body;
+    if (
+      data.email === undefined ||
+      data.username === undefined ||
+      data.password === undefined
+    ) {
+      const response = createResponse({
+        status: 403,
+        message: 'Data is null or empty',
+        data: null,
+      });
+      res.status(response.status);
+      res.send(response);
+      return console.log(chalk.red('Request data is null or empty'));
+    }
     if (!validator.isEmail(data.email)) {
       const response = createResponse({
         status: 403,
